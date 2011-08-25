cd9ccb9184fd5323f70d8fe136cea2d5f5184c64
yutax77 <yutax77@gmail.com>
2011-08-25 22:58:40 +0900

    P29

--- src/main/scala/com/yutax77/Dollar.scala
+++ src/main/scala/com/yutax77/Dollar.scala
@@ -1,19 +1,19 @@
 package com.yutax77
 
-class Money(var amount: Int)
+class Money(var amount: Int){
+  	override def equals(other: Any): Boolean =
+		other match {
+			case that: Money =>
+			  (that.isInstanceOf[Money]) &&
+			  that.amount == this.amount
+			case _ => false
+		}
+}
 
 class Dollar(amount: Int) extends Money(amount){
 	def times(multiplier: Int): Dollar = {
 	  return new Dollar(amount * multiplier)
 	}
-	
-	override def equals(other: Any): Boolean =
-		other match {
-			case that: Dollar =>
-			  (that.isInstanceOf[Dollar]) &&
-			  that.amount == this.amount
-			case _ => false
-		}
 }
 
 class Franc(var amount: Int) {
