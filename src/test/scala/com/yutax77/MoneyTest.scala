c3fb574bc183be34f36c0e090afa0a6bca6d9344
yutax77 <yutax77@gmail.com>
2011-08-25 23:29:34 +0900

    P37

--- src/test/scala/com/yutax77/MoneyTest.scala
+++ src/test/scala/com/yutax77/MoneyTest.scala
@@ -6,17 +6,17 @@ class MoneyTest extends TestNGSuite {
 	@Test
 	def testMultiplication() {
 	  val five = Money.dollar(5)
-	  expect(new Dollar(10)) {five.times(2)}
-	  expect(new Dollar(15)) {five.times(3)}
+	  expect(Money.dollar(10)) {five.times(2)}
+	  expect(Money.dollar(15)) {five.times(3)}
 	}
 	
 	@Test
 	def testEquality() {
-	  expect(true) {((new Dollar(5)) == (new Dollar(5)))}
-	  expect(false) {new Dollar(5) == new Dollar(6)}
+	  expect(true) {(Money.dollar(5) == Money.dollar(5))}
+	  expect(false) {Money.dollar(5) == Money.dollar(6)}
 	  expect(true) {((new Franc(5)) == (new Franc(5)))}
 	  expect(false) {new Franc(5) == new Franc(6)}
-	  expect(false) {new Franc(5) == new Dollar(5)}
+	  expect(false) {new Franc(5) == Money.dollar(5)}
 	}
 	
 	@Test
